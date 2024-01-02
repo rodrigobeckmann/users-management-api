f5ea7366a6b626e52f34f729f1d0df45f1d5ef0f
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-02 17:54:25 -0300

    feat: add common user validation on list user by id endpoint

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const userController = require('../controllers/users.controller');
 const adminValidation = require('../middlewares/adminValidation');
+const userValidation = require('../middlewares/userValidation');
 
 const usersRouter = Router();
 
 usersRouter.post('/', userController.createUser);
 usersRouter.get('/', adminValidation, userController.listAllUsers);
+usersRouter.get('/:id', userValidation, userController.getUserById);
 
 module.exports = usersRouter;
