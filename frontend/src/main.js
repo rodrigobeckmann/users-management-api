de75726dd02f33fe96ecd4638377842af5ff7573
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-03 13:53:30 -0300

    feat: config router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Login from './views/Login.vue'
+import ControlPanel from './views/ControlPanel.vue'
+import UserPanel from './views/UserPanel.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Login},
+  { path: '/control-panel', component: ControlPanel },
+  { path: '/user/:id', component: UserPanel},
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App)
+.use(router)
+.mount('#app')
