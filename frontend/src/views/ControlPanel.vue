41bc0a4d1ab6f03b1e562eeb27561496fd617f25
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-03 13:52:40 -0300

    feat: create contro panel view

---
+++ frontend/src/views/ControlPanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>cp</h1>
+  <button type="button" @click="logoff">Logoff</button>
+</template>
+
+<script setup>
+import {useRouter} from 'vue-router'
+
+const router = useRouter()
+
+const logoff = async () => {
+  const pastDate = new Date(0).toUTCString();
+  document.cookie = 'token=; expires=' + pastDate + '; path=/;';
+  router.push('/')
+}
+
+</script>
