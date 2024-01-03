651abf34984c8cd82b7448166eff7084266ac6f7
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-03 11:12:56 -0300

    feat: add API_URL env

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { getToken } from "../utils/token";
-const baseUrl = 'http://localhost:3001'
+const API_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
