fa6b49feabc13f0278c57807aec24db32d34bfd1
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-03 11:09:28 -0300

    feat: axios request should pass login token on headers

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-
+import { getToken } from "../utils/token";
 const baseUrl = 'http://localhost:3001'
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
+  headers: {
+    Authorization: `Bearer ${getToken()}`,
+  },
 });
 
 export default axiosInstance;
