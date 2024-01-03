1983adc12f435bf1eee0e0fd3be19eef4b289186
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-03 11:10:40 -0300

    feat: update main page to test functionalities

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { login, getAllUsers } from './services/login'
+import { onBeforeMount, ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
+const handleLogin = async (e) => {
+  e.preventDefault()
+  await login({email: email.value, password: password.value})
+}
+
+onBeforeMount(async () => {
+  const users = await getAllUsers()
+  console.log(users)
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <form :onSubmit="handleLogin">
+    <input v-model="email" type="text" placeholder="email" />
+    <input v-model="password" type="password" placeholder="password" />
+    <button type="submit">Login</button>
+  </form>
 </template>
 
 <style scoped>
