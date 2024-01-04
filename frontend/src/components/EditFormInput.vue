4ef4207d17cb6b368e8261e91146f721393e40cb
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-04 18:18:29 -0300

    feat: create component to use on form for edit user information and create user

---
+++ frontend/src/components/EditFormInput.vue
@@ -0,0 +1,11 @@
+<template>
+  <label> {{ inputName }}:
+    <input :disabled="!isEditing" class="bg-gray-300 text-black/50" :class="{ '!bg-white !text-black': isEditing }"
+      type="text" :value="value" @input="emit('input', $event.target.value)" />
+  </label>
+</template>
+
+<script setup>
+const props = defineProps(['isEditing', 'value', 'inputName'])
+const emit = defineEmits(['input'])
+</script>
