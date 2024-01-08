0c36720eb592aca582ac19ce8b20f967a36d6283
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-08 09:33:51 -0300

    feat: create base modal for edit picture action

---
+++ frontend/src/components/EditPictureModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div @click="emit('closeModal')" class="fixed inset-0 bg-gray-500/75">
+  <div class="flex size-full items-center justify-center">
+    <div @click.stop class="flex w-80 h-60 bg-white rounded ">
+
+    </div>
+  </div>
+  </div>
+
+  
+</template>
+
+<script setup>
+
+const emit = defineEmits(['closeModal'])
+
+</script>
