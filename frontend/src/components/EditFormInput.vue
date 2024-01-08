bb8f321dc0272f46d9f7267e94e52765d8aeaf90
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-08 19:28:53 -0300

    style: update style for edit form

--- frontend/src/components/EditFormInput.vue
+++ frontend/src/components/EditFormInput.vue
@@ -1,11 +1,13 @@
 <template>
-  <label> {{ inputName }}:
-    <input :disabled="!isEditing" class="bg-gray-300 text-black/50 rounded shadow-md mb-2" :class="{ '!bg-white !text-black': isEditing }"
-      type="text" :value="value" @input="emit('input', $event.target.value)" />
-  </label>
+  <div class="flex h-full w-full justify-center">
+    <div class="flex mb-2 items-center w-1/4 text-sm justify-center bg-blue-200 rounded-l border-l border-y border-solid border-gray-500 shadow-md text-center md:text-base">{{ inputName }}</div>
+      <input :disabled="!isEditing" class="bg-gray-300 text-black/50 rounded-r w-1/2 text-sm shadow-md mb-2 md:text-base"
+        :class="{ '!bg-white !text-black': isEditing }" :type="type || 'text'" :value="value"
+        @input="emit('input', $event.target.value)" />
+  </div>
 </template>
 
 <script setup>
-const props = defineProps(['isEditing', 'value', 'inputName'])
+const props = defineProps(['isEditing', 'value', 'inputName', 'type'])
 const emit = defineEmits(['input'])
 </script>
--- frontend/src/views/UserPanel.vue
+++ frontend/src/views/UserPanel.vue
@@ -2,17 +2,17 @@
 
   <Loading v-if="isLoading"/>
 
-  <div v-else class="flex flex-col w-full !h-auto lg:flex-row items-center justify-center mt-10 gap-10 md:mt-0 md:!size-full">
+  <div v-else class="flex flex-col w-full !h-auto lg:flex-row items-center justify-center mt-10 gap-10">
 
     
-    <div class="flex flex-col gap-4  bg-gray-300/50 w-80 h-3/4 rounded-md p-5 shadow border items-center">
+    <div class="flex flex-col gap-4 h-full  bg-gray-300/50 w-80 h-3/4 rounded-md p-5 shadow border items-center">
       <div class="flex w-full relative items-center justify-center">
         <div @click="toggleEditPicureModal" class="flex absolute hover:bg-slate-700/75 w-1/2 aspect-square rounded-full md:w-full items-center justify-center text-transparent cursor-pointer hover:text-black hover:shadow-md">Change Picture</div>
         <img class="w-1/2 aspect-square rounded-full object-cover shadow-md md:w-full" :src="user.userPicture">
       </div>
     </div>
 
-    <form class="flex flex-col gap-4  bg-gray-300/50 w-80 h-auto rounded-md p-5 shadow border md:w-[32rem] md:h-3/4">
+    <form class="flex flex-col gap-4 bg-gray-300/50 w-80 h-full rounded-md p-5 shadow border md:w-[32rem] md:h-auto">
 
       <EditFormInput :input-name="'First Name'" :is-editing="isEditing" :value="user.firstName" v-on:input="updateValue($event, 'firstName')"/>
 
@@ -22,7 +22,7 @@
       
       <EditFormInput :input-name="'Address'" :is-editing="isEditing" :value="user.address" v-on:input="updateValue($event, 'address')"/>
 
-      <EditFormInput :input-name="'Address Number'" :is-editing="isEditing" :value="user.addressNumber" v-on:input="updateValue($event, 'addressNumber')"/>
+      <EditFormInput :input-name="'Number'" :is-editing="isEditing" :value="user.addressNumber" :type="'number'" v-on:input="updateValue($event, 'addressNumber')"/>
 
       <EditFormInput :input-name="'Zip Code'" :is-editing="isEditing" :value="user.zipCode" v-on:input="updateValue($event, 'zipCode')"/>
 
@@ -32,8 +32,10 @@
 
       <EditFormInput :input-name="'Country'" :is-editing="isEditing" :value="user.country" v-on:input="updateValue($event, 'country')"/>
 
-      <button @click.prevent="toggleEdit">Edit</button>
-      <button type="submit" @click.prevent="handleEditUser">submit</button>
+      <div class="flex flex-col items-center">
+        <button class="mt-4 w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click.prevent="toggleEdit">Edit</button>
+        <button class="mt-4 w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" @click.prevent="handleEditUser">Save Changes</button>
+      </div>
     </form>
   </div>
 
@@ -68,7 +70,6 @@ const updateValue = (value, key) => {
 
 
 const handleEditUser = async () => {
-  console.log(user.value)
   const body = {
     firstName: user.value.firstName,
     lastName: user.value.lastName,
@@ -80,6 +81,7 @@ const handleEditUser = async () => {
     country: user.value.country
   }
   await updateUser(user.value.id, body)
+  toggleEdit()
 }
 
 const toggleEditPicureModal = () => {
