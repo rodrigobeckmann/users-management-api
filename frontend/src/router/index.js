e02050e4916ed8718b8a51a0d61784d50ed0687d
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2024-01-12 17:28:43 -0300

    feat: add route guard for all availble routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getLoggedUser } from '../services/login';
 
 const routes = [
-  { path: '/', component: () => import('../views/Login.vue') },
-  { path: '/control-panel', component: () => import('../views/ControlPanel.vue') },
-  { path: '/user/:id', component: () => import('../views/UserPanel.vue') },
+  { path: '/', component: () => import('../views/Login.vue'), meta: { verifyLogin: true } },
+  { path: '/control-panel', component: () => import('../views/ControlPanel.vue'), meta: { authAdmin: true } },
+  { path: '/user/:id', component: () => import('../views/UserPanel.vue'), props: true, meta: { authUser: true } },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+router.beforeEach( async (to, _from, next) => {
+  try {
+    const user = await getLoggedUser();
+    if (to.meta.verifyLogin && user.isAdmin) return next('/control-panel')
+    if (to.meta.verifyLogin && !user.isAdmin) return next('/user/' + user.id)
+    if (to.meta.authAdmin && user.isAdmin) return next()
+    if (to.meta.authUser && user.isAdmin || user.id === to.params.id) return next()
+    return next()
+  } catch {
+    return next('/')
+  }
+});
 
 export default router
